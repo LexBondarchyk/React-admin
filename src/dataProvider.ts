b7e25babb52589ca7c0152c32a600b4a3e9dd041
Oleksandr Bondarchuk <108199972+LexBondarchyk@users.noreply.github.com>
2023-07-26 03:31:41 +0300

    Update dataProvider.ts

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,3 +1,3 @@
-import jsonServerProvider from "ra-data-json-server";
+import jsonServerProvider from "https://github.com/marmelab/react-admin/tree/master/packages/ra-data-json-server";
 
 export const dataProvider = jsonServerProvider(import.meta.env.VITE_JSON_SERVER_URL);
